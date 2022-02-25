64577a312c3f02f92374457fda5f52ddb17d12cb
Riley Croker <riley.croker@vikings.berry.edu>
2022-02-25 15:20:23 -0500

    added a circle to the pygame window that moves as your hand moves in openCV

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # imports
 #############
 
+from ctypes.wintypes import HACCEL
 import cv2
+from cv2 import circle
 import pygame
 
 from HandDetector import HandDetector
@@ -22,11 +24,16 @@ pygame.display.set_caption("Best game")
 #game stuff
 FPS = 60
 
+######################
+## HELPER FUNCTIONS ##
+######################
 
+def mapToNewRange(val, inputMin, inputMax, outputMin, outputMax):
+    return outputMin + ((outputMax - outputMin) / (inputMax - inputMin)) * (val - inputMin)
 
-#########
-# main function
-#########
+###################
+## MAIN FUNCTION ##
+###################
 
 def main():
     # make a hand detector
@@ -34,21 +41,39 @@ def main():
 
     gameIsRunning  = True
 
+    # circle vars
+    circleX = 0
+    circleY = 0
+    circleZ = 0
+
     # while the opencv window is running
     while (not handDetector.shouldClose) and gameIsRunning:
         # update the webcam feed and hand tracker calculations
         handDetector.update()
 
+        WINDOW.fill((42,42,42))
+
         # if there is at least one hand seen, then
         # print out the landmark positions
         if len(handDetector.landmarkDictionary) > 0: 
-            print(handDetector.landmarkDictionary[0])
+            circleX = handDetector.landmarkDictionary[0][9][0]
+            circleY = handDetector.landmarkDictionary[0][9][1]
+            circleZ = abs(handDetector.landmarkDictionary[0][9][2])*5
+
+            circleX = WIDTH - mapToNewRange(circleX, 0, 640, 0, WIDTH)
+            circleY = mapToNewRange(circleY, 0, 480, 0, HEIGHT)
+
+            #draw circle at point 9
+            pygame.draw.circle(WINDOW, (0,255,255), (circleX,circleY), 50)
  
         # for all the game events
         for event in pygame.event.get():
             # quit game
             if event.type == pygame.QUIT:
                 gameIsRunning = False
+
+        # put code here that should be run every frame of your game             
+        pygame.display.update()
 
         
 
